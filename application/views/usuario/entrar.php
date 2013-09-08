9615574d1e62e7cbb24d89927187057099e50f6d
Rafael Couto <rafael@rafael-pc.(none)>
2013-09-08 19:09:53 -0300

    Retirado método antigo de login

--- application/views/usuario/entrar.php
+++ application/views/usuario/entrar.php
@@ -1,42 +1,4 @@
 <script type="text/javascript">
-// Quando carregado a página
-$(function($) {
- 
-    // Quando enviado o formulário
-    $('.form-horizontal').submit(function() {
- 
-        // Limpando mensagem de erro
-        $('div.mensagem-erro').html('');
- 
-        // Mostrando loader
-        $('div.loader').show();
- 
-        // Enviando informações do formulário via AJAX
-        $(this).ajaxSubmit(function(resposta) {
- 
-            // Se não retornado nenhum erro
-            if (!resposta)
-                // Redirecionando para o painel
-                window.location.href = '<?php echo site_url('usuario') ?>';
-            else
-            {
-                // Escondendo loader
-                $('div.loader').hide();
- 
-                // Exibimos a mensagem de erro
-                $('div.mensagem-erro').html(resposta);
-            }
- 
-        });
- 
-        // Retornando false para que o formulário não envie as informações da forma convencional
-        return false;
- 
-    });
-});
-</script>
-
-<script type="text/javascript">
     
     $(function($) {
       
